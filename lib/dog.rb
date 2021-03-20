1aa5ed526e1f74a643fcac2be0c6a3b55da961b7
Landon Reasoner <reasoner.landon90@gmail.com>
2021-03-20 17:33:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,4 +77,11 @@ class Dog
     end 
     dog 
   end 
+end
+
+def self.find_by_name(name:)
+  sql = ("SELECT * FROM dogs WHERE name = ? LIMIT 1")
+  DB[:conn].execute(sql, name).collect do |row|
+    self.new_from_db(row)
+  end.first
 end 
